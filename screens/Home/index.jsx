63fbb0461d56aabd0e54f02c36cf66b637d546ba
onurcelikel <onurcelikel@teknasyon.com>
2022-02-20 19:26:30 +0300

    Home search ui

--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -1,7 +1,63 @@
-import { Layout, Text } from '@ui-kitten/components';
+import React, { useState } from 'react';
+import { TouchableWithoutFeedback, StyleSheet, View } from 'react-native';
+import { Layout, Input, Divider, Button, Icon } from '@ui-kitten/components';
 
-export const Home = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>Home</Text>
-  </Layout>
-);
+export const Home = () => {
+  const [value, setValue] = useState('');
+
+  const IconList = (props) => (
+    <Icon {...props} name="grid"/>
+  );
+
+  const IconFilter = (props) => (
+    <Icon {...props} name="funnel"/>
+  );
+
+  const removeIcon = (props) => (
+    <TouchableWithoutFeedback onPress={() => setValue('')}>
+      <Icon {...props} name="close-circle"/>
+    </TouchableWithoutFeedback>
+  );
+
+
+
+  return (
+    <Layout style={styles.layout}>
+      <View style={styles.searchBox}>
+        <Input
+          placeholder='Search'
+          value={value}
+          style={styles.searchInput}
+          accessoryRight={value.length ? removeIcon : ''}
+          onChangeText={nextValue => setValue(nextValue)}
+        />
+        <Button style={styles.button} accessoryLeft={IconList} />
+        <Button style={styles.button} accessoryLeft={IconFilter} />
+      </View>
+      <Divider />
+    </Layout>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    padding: 10,
+  },
+  searchInput: {
+    width: '100%',
+    flex: 1,
+    marginBottom: 10
+  },
+  searchBox: {
+    flexDirection: 'row'
+  },
+  button: {
+    height: 20,
+    width: 30,
+    marginLeft: 5
+  },
+  removeIcon: {
+  }
+});
